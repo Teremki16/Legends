e932092d012c8758a77675ad5666c45b2ab5eab7
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2024-06-09 15:49:12 +0300

    Ми налаштували кнопки, зберігання, редагування, архівування

--- script.js
+++ script.js
@@ -1,46 +1,84 @@
-$(document).ready(function () {
-  let notes = []
 
+let notes = JSON.parse(localStorage.getItem("notes")) || []
+draw()
 
-  $("#add").click(function(){
-    let note = {
-      title: $(".title").val(),
-      text: $(".text").val(),
-      data: new Date().getTime()
-    }
-    notes.push(note)
-    $(".title").val("")
-    $(".text").val("")
-    console.log(notes)
-    draw()
-  })
+$("#add").click(function () {
+  let note = {
+    title: $(".title").val(),
+    text: $(".text").val(),
+    data: new Date().getTime(),
+    complete: false
+  }
+  notes.push(note)
+  $(".title").val("")
+  $(".text").val("")
+  localStorage.setItem("notes", JSON.stringify(notes))
+  draw()
+  
+})
 
-  function draw(){
-    notes.forEach(element => {
-      $(".notes").append(
-        `
-        <div class="note">
+function draw() {
+  $(".notes").empty()
+  notes.forEach(element => {
+    $(".notes").append(
+      `<div class="note ${element.complete}">
       <div class="head">
-        <h3>SIT</h3>
-        <h5>12:11</h5>
+        <h3>${element.title}</h3>
+        <h5>${new Date(element.data).toDateString()}</h5>
       </div>
 
-      <p> Введіть інформацію котру ви хочете прикріпити або запам'ятати</p>
+      <p>${element.text}</p>
+      
       <div class="buttons">
-        <button class="but"><i class="fa-solid fa-pen"></i></button>
-        <button><i class="fa-solid fa-trash-can"></i></button>
-        <button><i class="fa-solid fa-check"></i></button>
+        <button onclick="redact(${element.data})"><i class="fa-solid fa-pen"></i></button>
+        <button onclick="del(${element.data})"><i class="fa-solid fa-trash-can"></i></button>
+        <button onclick="noteDo(${element.data})"><i class="fa-solid fa-check"></i></button>
       </div>
 
     </div>
         `
-      )
-    });
-  }
+    )
+    
+  });
+}
 
-});
-// new Date(data).toDateString()
+function del(data) {
+  notes = notes.filter(item => item.data !== data)
+  localStorage.setItem("notes", JSON.stringify(notes))
+  draw()
+}
+
+function noteDo(complete) {
+  
+  let objectToModify = notes.find(obj => obj.data == complete);
+
+  // Змінюємо поле обраного об'єкта
+  if (objectToModify) {
+    objectToModify.complete = !objectToModify.complete;
+  }
+  localStorage.setItem("notes", JSON.stringify(notes))
+  draw()
+
+}
+function redact(data) {
+  let objectToModify = notes.find(obj => obj.data == data);
+  
+
+  // Змінюємо поле обраного об'єкта
+  if (objectToModify) {
+    objectToModify.title = prompt("Введіть новий заголовок",objectToModify.title)
+    objectToModify.text = prompt("Введіть новий текст",objectToModify.text)
+  }
+  localStorage.setItem("notes", JSON.stringify(notes))
+  draw()
+
+}
+
+// prompt("asdasd","" ,(mess)=>{
+//   alert(mess)
+// })
+
+
 
 // localStorage.setItem("notes", JSON.stringify(data))
 
-// JSON.parse(localStorage.getItem("notes"))
